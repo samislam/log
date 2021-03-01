9e7e5c0c44d1300fd56596bfe425967270527237
samislam <samerjojo15.sy@gmail.com>
2021-03-01 22:03:43 +0300

    commited the version 1.1.0

--- index.js
+++ index.js
@@ -1,30 +1,76 @@
+// ============ PRIVATE ============
 const colors = require("colors/safe");
-colors.setTheme({
-  error: "red",
-  warn: "yellow",
-  success: "green",
-  info: "cyan",
-  default: "reset"
-});
 
-function log(args, type) {
-  const label = colors.bold(`${type.charAt(0).toUpperCase()}${type.slice(1)}:`);
-  if (args[0] === api.label) args[0] = label;
-  const x = args.map((arg) => colors[type](arg));
-  console.log(...x);
-  // return "";
-}
-
-const api = {
-  warn: (...args) => log(args, "warn"),
-  error: (...args) => log(args, "error"),
-  success: (...args) => log(args, "success"),
-  info: (...args) => log(args, "info"),
-  default: (...args) => log(args, "default"),
-  label: {
-    info:
-      "You should only use log.label as the first argument in log.X(), if you need to the label in somewhere else, use log.X.name, For Example: log.warn.name",
-  },
+const engine = (label, key, ...message) => {
+  const coloredLabel = colors.bold(label);
+  if (message[0] === log.label) message[0] = coloredLabel;
+  const coloredMessages = message.map((el) => {
+    return colors[key](el);
+  });
+  console.log(...coloredMessages);
 };
 
-module.exports = api;
+const setTheme = (key, value, label = capitalize(key)) => {
+  if (!key || !value) throw new Error("Syntax: log.set('myName', 'red')");
+  theme[key] = value;
+  colors.setTheme(theme);
+  log[key] = (...message) => {
+    engine(label, key, ...message);
+  };
+};
+
+const capitalize = (s) => {
+  return s && s[0].toUpperCase() + s.slice(1);
+};
+
+const theme = {};
+// ============ PUBLIC ============
+
+const log = function (...message) {
+  if (message.length != 0) console.log(...message);
+};
+Object.defineProperty(log, "label", {
+  enumerable: false,
+  value: Symbol(),
+  configurable: false,
+  // writable: false,
+});
+Object.defineProperty(log, "set", {
+  enumerable: false,
+  value (key, value, label) {
+    setTheme(key, value, label);
+  },
+  configurable: false,
+  // writable: false,
+});
+
+// PRE-DEFINED THEMES
+
+const pre = {
+  error: ["red", "Error:"],
+  err: ["red", "Error:"],
+  e: ["red", "Error:"],
+  f: ["red", "Error:"],
+  fail: ["red", "Fail:"],
+  faild: ["red", "Faild:"],
+  danger: ["red", "Danger:"],
+  success: ["green", "Success:"],
+  ok: ["green", "OK:"],
+  s: ["green", "Success:"],
+  w: ["yellow", "Warning:"],
+  warn: ["yellow", "Warning:"],
+  warning: ["yellow", "Warning:"],
+  note: ["yellow", "Note: "],
+  i: ["cyan", "info:"],
+  info: ["cyan", "info:"],
+  information: ["cyan", "information:"],
+  done: ["cyan", "Done!"],
+};
+
+for ([key, value] of Object.entries(pre)) {
+  const style = value[0];
+  const label = value[1];
+  log.set(key, style, label);
+}
+
+module.exports = log;
--- tests/test.js
+++
@@ -1,9 +0,0 @@
-const log = require("../index");
-
-log.info("here is an information message");
-log.warn("here is an warning message");
-log.error("here is an error message");
-log.success("here is an success message");
-log.default("here is a default message");
-
-log.error(log.label, "Could Not Connect To Database");
